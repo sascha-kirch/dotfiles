270be782646f27b36534c76f820c3c6d5347c8e0
sascha-kirch <skirch1@alumno.uned.es>
2025-08-25 10:12:14 +0200

    add behavior specifically for vscode

--- neofetch/.bashrc.d/90-neofetch.sh
+++ neofetch/.bashrc.d/90-neofetch.sh
@@ -1,4 +1,7 @@
-# Execute neofetch only if not inside TMUX and if using an interactive shell (not if only running scripts or ssh commands)
-if [ -z "$TMUX" ] && [ -n "$PS1" ]; then
+# Execute neofetch only if:
+# - not inside TMUX and
+# - if using an interactive shell (not if only running scripts or ssh commands)
+# - if not inside VSCode
+if [ -z "$TMUX" ] && [ -n "$PS1" ] && [ "$TERM_PROGRAM" != "vscode" ]; then
     neofetch
 fi
--- tmux/.bashrc.d/91-tmux.sh
+++ tmux/.bashrc.d/91-tmux.sh
@@ -1,8 +1,17 @@
-
 # Start a tmux session if not already inside one
 if [ -n "$PS1" ] && [ -z "$TMUX" ]; then
 
-    # Run neofetch only when starting a new tmux session and redirect errors to /dev/null (e.g. not installed)
-    # Otherwise, just run bash
-    tmux new-session -A -s main "neofetch 2> /dev/null; bash"
+    # Check if inside VSCode terminal. The $TERM_PROGRAM variable is set to "vscode" in that case
+    if [ "$TERM_PROGRAM" = "vscode" ]; then
+        # If inside VSCode, create or attach to a tmux session named after the current folder. Which will be the workspace folder
+        # opened in VSCode. This way each VSCode workspace can have its own tmux session.
+        SESSEION_NAME="vscode-$(basename "$PWD")"
+        tmux new-session -A -s "$SESSEION_NAME"
+    else
+        # If not inside VSCode, do the following:
+        # Run neofetch only when starting a new tmux session and redirect errors to /dev/null (e.g. not installed)
+        # Otherwise, just run bash
+        tmux new-session -A -s main "neofetch 2> /dev/null; bash"
+    fi
+
 fi
