14b787532c473eabe308077e72d0d15b7be0c691
sascha-kirch <skirch1@alumno.uned.es>
2025-08-25 11:07:09 +0200

    avoid starting tmux as login shell

--- tmux/.bashrc.d/91-tmux.sh
+++ tmux/.bashrc.d/91-tmux.sh
@@ -5,8 +5,11 @@ if [ -n "$PS1" ] && [ -z "$TMUX" ]; then
     if [ "$TERM_PROGRAM" = "vscode" ]; then
         # If inside VSCode, create or attach to a tmux session named after the current folder. Which will be the workspace folder
         # opened in VSCode. This way each VSCode workspace can have its own tmux session.
-        SESSEION_NAME="vscode-$(basename "$PWD")"
-        tmux new-session -A -s "$SESSEION_NAME"
+        PROJ_DIR="$PWD"
+        SESSEION_NAME="vscode-$(basename "$PROJ_DIR")"
+        echo "You are inside VSCode."
+        echo "Starting or attaching to tmux session '$SESSEION_NAME' for project directory '$PROJ_DIR'"
+        tmux new-session -A -s "$SESSEION_NAME" -c "$PROJ_DIR"
     else
         # If not inside VSCode, do the following:
         # Run neofetch only when starting a new tmux session and redirect errors to /dev/null (e.g. not installed)
