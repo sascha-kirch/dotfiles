184a220fa61d3da61f440244f1807ce6b6df5500
sascha-kirch <skirch1@alumno.uned.es>
2025-08-31 22:35:11 +0200

    add vi mode keybindings

---
+++ bash/.bashrc.d/00-keybindings.sh
@@ -0,0 +1,10 @@
+# Use vi keybindings in bash
+set -o vi
+
+# Clear screen with Ctrl+l in both command and insert modes
+bind -m vi-command 'Control-l: clear-screen'
+bind -m vi-insert 'Control-l: clear-screen'
+
+# Set cursor shape for command mode (block) and insert mode (beam)
+bind -m vi-command 'set vi-cmd-mode-string "\1\e[2 q\2"'
+bind -m vi-command 'set vi-ins-mode-string "\1\e[6 q\2"'
