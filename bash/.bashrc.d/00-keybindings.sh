0ba7564bd8c5ae9c622de82e369557d0aa2e23fb
sascha-kirch <skirch1@alumno.uned.es>
2025-09-01 21:42:34 +0200

    fix vi mode indicator

--- bash/.bashrc.d/00-keybindings.sh
+++ bash/.bashrc.d/00-keybindings.sh
@@ -6,5 +6,6 @@ bind -m vi-command 'Control-l: clear-screen'
 bind -m vi-insert 'Control-l: clear-screen'
 
 # Set cursor shape for command mode (block) and insert mode (beam)
+bind -m vi-command 'set show-mode-in-prompt on'
 bind -m vi-command 'set vi-cmd-mode-string "\1\e[2 q\2"'
 bind -m vi-command 'set vi-ins-mode-string "\1\e[6 q\2"'
