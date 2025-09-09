4b954bcb8aa8c1d886a2dde682ecfbb53536873c
sascha-kirch <skirch1@alumno.uned.es>
2025-09-09 09:17:38 +0200

    add nvim configs

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed ={"c", "cpp", "vim", "lua", "markdown", "python", "rust", "bash"},
+      highlight = {enable=true},
+      indent = {enable = true},
+    })
+  end
+}
--- profile/.profile.d/paths.sh
+++ profile/.profile.d/paths.sh
@@ -5,3 +5,7 @@ fi
 if [ -d "$HOME/.local/bin" ] ;then
     PATH="$HOME/.local/bin:$PATH"
 fi
+
+if [ -d "$HOME/.local/nvim-linux-x86_64/bin" ] ;then
+    PATH="$HOME/.local/nvim-linux-x86_64/bin:$PATH"
+fi
