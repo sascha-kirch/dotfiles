222718b70ab615053a669cfdc209a98d0f2189d1
sascha-kirch <skirch1@alumno.uned.es>
2025-09-10 10:41:44 +0200

    add aliases

--- bash/.bashrc.d/00-aliases.sh
+++ bash/.bashrc.d/00-aliases.sh
@@ -1,2 +1,10 @@
 # Package Manager
 alias aptup='sudo apt update && sudo apt upgrade -y'
+
+
+# ffmpeg
+alias heic2png='find . -maxdepth 1 -type f -iname "*.heic" -print0 | xargs -0 -I{} sh -c '\''magick convert "$1" "${1%.*}.png"'\'' _ {}'
+
+
+# Storage
+alias duh='du -h --max-depth=1 . | sort -hr'
