a49f1564b9b6f681a07f30009c2485aed4afd39d
sascha-kirch <skirch1@alumno.uned.es>
2025-09-11 14:35:23 +0200

    initial lsp support

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls","basedpyright"}
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.basedpyright.setup({})
+
+      -- following some example keymaps. for more info enter
+      -- comand :help vim.lsp.buf
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition,{})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action,{})
+    end
+  },
+}
