df178acf6c4d0778e2da306e31f580e72705d792
sascha-kirch <skirch1@alumno.uned.es>
2025-09-12 12:44:53 +0200

    add nn-ls and lua formatter

---
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,18 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				-- WARN: Don't forget to install formaters/linters w. Mason
+				-- or in lsp-config under ensure_installed
+				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.ruff,
+				-- null_ls.builtins.diagnostics.ruff,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
