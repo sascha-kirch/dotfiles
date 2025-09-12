3200fbc0538244dc9edf48b7479f25d50a72538b
sascha-kirch <skirch1@alumno.uned.es>
2025-09-12 10:07:52 +0200

    add todo comments

---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,18 @@
+-- TODO: this is a todo
+-- over multiple lines!!!
+
+-- normal comment
+
+-- HACK: thats hacky
+-- WARN: caution here
+-- PERF: too sloooooow
+-- NOTE: a little note
+-- TEST: just for testing
+-- FIX: need to fix this one!
+-- BUG: oooh a bug!
+
+return {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true }
+}
