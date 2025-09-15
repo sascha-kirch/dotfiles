e8626ad1205c481f74df1f22e0b638b6e445147f
sascha-kirch <skirch1@alumno.uned.es>
2025-09-15 11:02:43 +0200

    update config for todo comments

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -15,4 +15,32 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { signs = true },
+    config = function()
+        local todo_comments = require("todo-comments")
+        todo_comments.setup({})
+
+        ------------------
+        -- Default keymaps
+        ------------------
+
+        vim.keymap.set("n", "]t", function()
+            todo_comments.jump_next()
+        end, { desc = "Next todo comment" })
+
+        vim.keymap.set("n", "[t", function()
+            todo_comments.jump_prev()
+        end, { desc = "Previous todo comment" })
+
+        -- You can also specify a list of valid jump keywords
+        -- vim.keymap.set("n", "]t", function()
+        --     require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
+        -- end, { desc = "Next error/warning todo comment" }) 
+
+        -----------------
+        -- custom keymaps
+        -----------------
+
+        vim.keymap.set("n", "<leader>tl","<cmd>TodoQuickFix<CR>" , {desc="Show the [t]odo [l]ist"})
+        vim.keymap.set("n", "<leader>tf","<cmd>TodoTelescope<CR>" , {desc="Search todo list with telescope."})
+    end
 }
