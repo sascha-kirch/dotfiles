09824b837490cdeeec28aa0d13a54a260f4ca689
sascha-kirch <skirch1@alumno.uned.es>
2025-09-16 13:09:17 +0200

    remove paths to individual packages

--- profile/.profile.d/paths.sh
+++ profile/.profile.d/paths.sh
@@ -5,7 +5,3 @@ fi
 if [ -d "$HOME/.local/bin" ] ;then
     PATH="$HOME/.local/bin:$PATH"
 fi
-
-if [ -d "$HOME/.local/nvim-linux-x86_64/bin" ] ;then
-    PATH="$HOME/.local/nvim-linux-x86_64/bin:$PATH"
-fi
