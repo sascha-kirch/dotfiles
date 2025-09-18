c6d19e9fd8ba6f0736df9cc368b19f271aee1e42
sascha-kirch <skirch1@alumno.uned.es>
2025-09-18 10:35:20 +0200

    add config for vscode

---
+++ nvim/.config/nvim/lua/config_vscode.lua
@@ -0,0 +1,13 @@
+
+-- if inside vscode using the vscode-neovim extension, don't load plugins
+
+-- Load settings and keymaps. Note that not all settings and keybindings are compatible with vscode
+require("keymaps")
+require("vim-options")
+require("autocmds")
+
+-- In vscode, we need to show the mode since the statusline is not visible all the time
+vim.o.showmode = true
+
+local vscode = require("vscode")
+vscode.notify("Neovim is running inside VSCode", "info", { title = "vscode-neovim" })
