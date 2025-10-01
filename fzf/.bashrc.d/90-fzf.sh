21d42a984090661df3550e8444b84204244ae029
sascha-kirch <skirch1@alumno.uned.es>
2025-10-01 20:28:14 +0200

    add if guard

--- fzf/.bashrc.d/90-fzf.sh
+++ fzf/.bashrc.d/90-fzf.sh
@@ -1,12 +1,13 @@
+if command -v fzf &> /dev/null; then
+    export FZF_DEFAULT_OPTS="--layout=reverse --exact --border=bold --border=rounded --margin=3% --color=dark"
 
-export FZF_DEFAULT_OPTS="--layout=reverse --exact --border=bold --border=rounded --margin=3% --color=dark"
+    alias fzfm='fzf --multi'
+    alias fzfp='fzf --preview "cat -n {}"'
+    alias fzfmp='fzf --multi --preview "cat -n {}"'
 
-alias fzfm='fzf --multi'
-alias fzfp='fzf --preview "cat -n {}"'
-alias fzfmp='fzf --multi --preview "cat -n {}"'
-
-# Enables the following keybindings:
-# CTRL-t = fzf select
-# CTRL-r = fzf history
-# ALT-c  = fzf cd
-eval "$(fzf --bash)"
+    # Enables the following keybindings:
+    # CTRL-t = fzf select
+    # CTRL-r = fzf history
+    # ALT-c  = fzf cd
+    eval "$(fzf --bash)"
+fi
