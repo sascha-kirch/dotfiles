239e18cf32881d102aab050a6b4728b7c055ec0d
sascha-kirch <skirch1@alumno.uned.es>
2025-10-07 10:05:42 +0200

    add ruff alias

--- bash/.bashrc.d/00-aliases.sh
+++ bash/.bashrc.d/00-aliases.sh
@@ -8,3 +8,6 @@ alias heic2png='find . -maxdepth 1 -type f -iname "*.heic" -print0 | xargs -0 -I
 
 # Storage
 alias duh='du -h --max-depth=1 . | sort -hr'
+
+# ruff python linter & formatter
+alias myruff='ruff format -q --force-exclude --exclude "*.ipynb" && ruff check --fix -q -e --output-format grouped && ruff check --statistics -q'
