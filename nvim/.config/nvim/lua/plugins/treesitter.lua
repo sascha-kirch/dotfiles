b2ba1e70c0c61eec174990ed21f69c277682c9e0
sascha-kirch <skirch1@alumno.uned.es>
2025-10-14 16:22:50 +0200

    change keybinding and add treesitter textobjects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,53 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "c", "cpp", "vim", "lua", "markdown", "python", "rust", "bash" },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                ensure_installed = { "c", "cpp", "vim", "lua", "markdown", "python", "rust", "bash" },
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        after = "nvim-treesitter",
+        requires = "nvim-treesitter/nvim-treesitter",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        lookbehind = true,
+
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["ab"] = "@block.outer",
+                            ["ib"] = "@block.inner",
+
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+
+                            ["ai"] = "@conditional.outer",
+                            ["ii"] = "@conditional.inner",
+
+                            ["al"] = "@loop.outer",
+                            ["il"] = "@loop.inner",
+
+                            ["as"] = "@local.scope",
+                        },
+                        selection_modes = { ["default"] = "V"}, -- linewise 
+                    },
+                },
+            })
+        end,
+    },
 }
