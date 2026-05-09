1f62237d6396ad5941fd1f35ac44b1dcaa18ed3e
sascha-kirch <skirch1@alumno.uned.es>
2026-05-09 19:54:05 +0200

    skip tmux activation in vscode

--- tmux/.bashrc.d/91-tmux.sh
+++ tmux/.bashrc.d/91-tmux.sh
@@ -5,17 +5,20 @@ if [ -n "$PS1" ] && [ -z "$TMUX" ]; then
     if [ "$TERM_PROGRAM" = "vscode" ]; then
         # If inside VSCode, create or attach to a tmux session named after the current folder. Which will be the workspace folder
         # opened in VSCode. This way each VSCode workspace can have its own tmux session.
-        SESSEION_NAME="vscode-$(basename "$PWD")"
+        # SESSEION_NAME="vscode-$(basename "$PWD")"
+        # echo "You are inside VSCode."
+        # echo "Starting or attaching to tmux session '$SESSEION_NAME'"
+        # tmux new-session -A -s "$SESSEION_NAME"
         echo "You are inside VSCode."
-        echo "Starting or attaching to tmux session '$SESSEION_NAME'"
-        tmux new-session -A -s "$SESSEION_NAME"
+        echo "Tmux will not be started automatically. You can start it with 'tmux new-session -A -s <session-name>'"
     else
         # If not inside VSCode, do the following:
         # Run neofetch only when starting a new tmux session and redirect errors to /dev/null (e.g. not installed)
         # Otherwise, just run bash
         # tmux new-session -A -s main "neofetch 2> /dev/null; bash"
 
-        tmux new-session -A -s main
+        tmux new-session -A -s Root-Session
     fi
+
 
 fi
